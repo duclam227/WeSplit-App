a7142b7996b5208c8129e659fd0e7fde991b0e9a
Kim Nguyễn Duy Thiên <18120047@student.hcmus.edu.vn>
2020-12-03 21:20:30 +0700

    Changes and update to DTO_Trip.cs

--- WeSplit/DTO_WeSplit/DTO_Trip.cs
+++ WeSplit/DTO_WeSplit/DTO_Trip.cs
@@ -8,20 +8,28 @@ namespace DTO_WeSplit
 {
     class DTO_Trip
     {
+
+        #region Fields
         private int _tripId;
         private string _tripName;
         private string _tripDate;
         private string _tripDescription;
         private double _tripBudget;
         private double _tripAverage;
+        private List<DTO_Place> _tripDestinationList;
+        #endregion
 
+        #region Public Properties
         public int TripId { get => _tripId; set => _tripId = value; }
         public string TripName { get => _tripName; set => _tripName = value; }
         public string TripDate { get => _tripDate; set => _tripDate = value; }
         public string TripDescription { get => _tripDescription; set => _tripDescription = value; }
         public double TripBudget { get => _tripBudget; set => _tripBudget = value; }
         public double TripAverage { get => _tripAverage; set => _tripAverage = value; }
+        public List<DTO_Place> TripDestinationList { get => _tripDestinationList; } 
+        #endregion
 
+        #region Constructor
         public DTO_Trip()
         {
 
@@ -35,6 +43,9 @@ namespace DTO_WeSplit
             TripDescription = description;
             TripBudget = budget;
             TripAverage = average;
-        }
+        } 
+        #endregion
+
+
     }
 }
